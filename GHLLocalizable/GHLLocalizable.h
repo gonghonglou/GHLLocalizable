3828ca00e54dc18bf1ebb512230c18bdb559e6ec
gonghonglou <gonghonglou@gmail.com>
2018-03-15 00:04:03 +0800

    first commit

--- GHLLocalizable/GHLLocalizable.h
+++ GHLLocalizable/GHLLocalizable.h
@@ -6,14 +6,31 @@
 //  Copyright © 2018年 Troy. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
-//! Project version number for GHLLocalizable.
-FOUNDATION_EXPORT double GHLLocalizableVersionNumber;
+// 简体中文
+#define GHLChinese_Hans @"zh-Hans"
+// 繁体中文
+#define GHLChinese_Hant @"zh-Hant"
+// 英文
+#define GHL_English @"en"
 
-//! Project version string for GHLLocalizable.
-FOUNDATION_EXPORT const unsigned char GHLLocalizableVersionString[];
+#define GHLNotificationLanguageChanged @"languageChanged"
 
-// In this header, you should import all the public headers of your framework using statements like #import <GHLLocalizable/PublicHeader.h>
+#define GHLLanguageKey @"language"
 
+#define CurrentLanguageIsChinese ([[GHLInternationalControl chineseHans] containsObject:[GHLLocalizable userLanguage]])
 
+#import <Foundation/Foundation.h>
+
+@interface GHLLocalizable : NSObject
+
++ (void)initUserLanguage;
+
+// 设置当前语言
++ (void)setUserlanguage:(NSString *)language;
+
+// 获取应用当前语言
++ (NSString *)userLanguage;
+
+@end
---
+++ GHLLocalizable/NSBundle+GHLLanguage.h
@@ -0,0 +1,15 @@
+//
+//  NSBundle+GHLLanguage.h
+//  GHLLocalizable
+//
+//  Created by gonghonglou on 2018/3/14.
+//  Copyright © 2018年 Troy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSBundle (GHLLanguage)
+
++ (void)setLanguage:(NSString *)language;
+
+@end
