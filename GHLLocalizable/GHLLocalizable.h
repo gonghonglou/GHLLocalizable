08836e777e6174aa813fd8701a657a2737176ccc
gonghonglou <gonghonglou@gmail.com>
2018-03-14 23:52:28 +0800

    Initial Commit

---
+++ GHLLocalizable/GHLLocalizable.h
@@ -0,0 +1,19 @@
+//
+//  GHLLocalizable.h
+//  GHLLocalizable
+//
+//  Created by gonghonglou on 2018/3/14.
+//  Copyright © 2018年 Troy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GHLLocalizable.
+FOUNDATION_EXPORT double GHLLocalizableVersionNumber;
+
+//! Project version string for GHLLocalizable.
+FOUNDATION_EXPORT const unsigned char GHLLocalizableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GHLLocalizable/PublicHeader.h>
+
+
